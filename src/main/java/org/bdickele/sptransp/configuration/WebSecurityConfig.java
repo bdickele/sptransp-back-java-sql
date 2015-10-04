1ae9f2826f5e9c26ccc8a39084408ffc2a67ec96
Bertrand DICKELE <bdickele@gmail.com>
2015-10-04 21:53:34 +0200

    WebSecurity config

--- src/main/java/org/bdickele/sptransp/configuration/WebSecurityConfig.java
+++ src/main/java/org/bdickele/sptransp/configuration/WebSecurityConfig.java
@@ -1,20 +1,28 @@
 package org.bdickele.sptransp.configuration;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebMvcSecurity
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -25,26 +33,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 //.antMatchers("/").permitAll().and()
-                .anyRequest().permitAll()
-                .and().authorizeRequests().antMatchers("/console/**").permitAll();
-
-        // Because of H2 database
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
+                .anyRequest().permitAll();
     }
-        /*
-        http.formLogin()
-                .and().logout().and().authorizeRequests()
-                .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll().anyRequest()
-                .authenticated().and().csrf()
-                .csrfTokenRepository(csrfTokenRepository()).and()
-                .addFilterAfter(new CrossScriptRequestForgeryFilter(), CsrfFilter.class);
-                */
 
     private CsrfTokenRepository csrfTokenRepository() {
         HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
         repository.setHeaderName("X-XSRF-TOKEN");
         return repository;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select uid_user, password, 1 from ST_USER where uid_user=?")
+                .authoritiesByUsernameQuery(
+                        "select u.uid_user, r.user_role " +
+                        "from st_user u, st_profile_role r " +
+                        "where u.uid_user=? " +
+                        "and u.user_profile = r.user_profile")
+                .passwordEncoder(new StandardPasswordEncoder("53cr3t"));
     }
 
     /*
