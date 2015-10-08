984cddcb2dd39348a60e467c00343d88d0567392
Bertrand DICKELE <bdickele@gmail.com>
2015-10-08 21:18:52 +0200

    Working on UID generation

--- src/main/java/org/bdickele/sptransp/service/UserService.java
+++ src/main/java/org/bdickele/sptransp/service/UserService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,31 +31,48 @@ public class UserService extends AbstractService {
         int length = fullName.length();
         String prefix = fullName.substring(0, length < UID_PREFIX_LENGTH ? length : UID_PREFIX_LENGTH);
 
+        // If prefix is "john" that repository method may return "john1", "john2" and "johndoe3"
+        // That way we can perform a first filter
         List<String> uidsWithSamePrefix = repository.findUidsStartingWith(prefix);
 
-        //TODO
+        Optional<Integer> maxIndex = getMaxIndexOfUidsWithSamePrefix(uidsWithSamePrefix, prefix);
 
-        List<Integer> suffixes = new ArrayList<>();
-
-        return null;
+        return prefix + maxIndex.orElse(0) + 1;
     }
 
     /**
      * If prefix is equal to "john" and :<ul>
-     *     <li>uid = "john12", then method returs Some(12)</li>
-     *     <li>uid = "johndoe12", then method returs Optional.empty</li>
+     *     <li>uid = "john12", then method returs Optional.of(12)</li>
+     *     <li>uid = "johndoe12", then method returns Optional.empty</li>
      * </ul>
      * @param uid
      * @param prefix
      * @return
      */
     public static Optional<Integer> extractSuffixNumberFromUid(String uid, String prefix) {
-        String suffix = uid.substring(prefix.length());
+        if (!uid.startsWith(prefix)) return Optional.empty();
+
         try {
+            String suffix = uid.substring(prefix.length());
             Integer i = Integer.valueOf(suffix);
             return Optional.of(i);
         } catch (NumberFormatException e) {
             return Optional.empty();
         }
+    }
+
+    /**
+     * Highest index of uids with exactly the same prefix./ For instance if prefix = "john", and list is
+     * made of "john1", "john2" and "johnny5", then highest index is 2 (johndoe has not exactly the same prefix)
+     * @param uids List of UIDs, for instance john3
+     * @param prefix Prefix of UID (what is before the number)
+     * @return Optional with max index. Optional is empty if there is no uid with exactly the same prefix.
+     */
+    public static Optional<Integer> getMaxIndexOfUidsWithSamePrefix(List<String> uids, String prefix) {
+        return uids.stream()
+                .map(u -> extractSuffixNumberFromUid(u, prefix))
+                .filter(o -> o.isPresent())
+                .map(Optional::get)
+                .max(Comparator.<Integer>naturalOrder());
     }
 }
--- src/test/java/org/bdickele/sptransp/service/UserServiceTest.java
+++ src/test/java/org/bdickele/sptransp/service/UserServiceTest.java
@@ -1,7 +1,12 @@
 package org.bdickele.sptransp.service;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,7 +14,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Created by Bertrand DICKELE
  */
+@RunWith(JUnitParamsRunner.class)
 public class UserServiceTest {
+
+    private static List<String> UIDS = Arrays.asList("john1", "john2", "john3", "johndoe4");
+
 
     @Test
     public void suffix_extracting_should_work() {
@@ -19,5 +28,22 @@ public class UserServiceTest {
 
         result = UserService.extractSuffixNumberFromUid("john12", prefix);
         assertThat(result).contains(12);
+    }
+
+    public Object[][] indexExtractionProvider() {
+        return new Object[][] {
+                {UIDS, "john", Optional.of(3)},
+                {UIDS, "johndoe", Optional.of(4)},
+                {UIDS, "johndoee", Optional.empty()},
+                {UIDS, "joh", Optional.empty()}
+        };
+    }
+
+    @Test
+    @Parameters(method = "indexExtractionProvider")
+    public void get_max_index_of_suffixes_should_return_a_value(List<String> uids, String prefix,
+                                                                Optional<Integer> expectedResult) {
+        Optional<Integer> max = UserService.getMaxIndexOfUidsWithSamePrefix(uids, prefix);
+        assertThat(max).isEqualTo(expectedResult);
     }
 }
